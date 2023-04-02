a2bcb3d15933246bc8e72f027313a4f2e8c9b028
Arman Rahman <armanrahman0@gmail.com>
2023-04-02 23:55:56 +0600

    u

---
+++ assets/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+	<!-- CSS files -->
+	<link rel="stylesheet" type="text/css" href="./assects/css/style.css">
+	<link rel="stylesheet" type="text/css" href="./assects/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="./assects/css/fontawesome-all.min.css">
+	
+</head>
+<body>
+	<div class="container">
+       <div class="row">
+        <div class="col-6">
+            asdasd
+        </div>
+       </div>
+    </div>
+</body>
+<!-- JS files -->
+	<script src="./assects/js/jquery-3.6.4.js"></script>
+	<script src="./assects/js/bootstrap.js"></script>
+	<script src="./assects/js/fontawesome-all.min.js"></script>
+	<script src="./assects/js/script.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>My Website</title>
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<!-- SEO -->
+
+	<!-- These tags help optimize the webpage for social sharing by specifying the title, description, and image that should be used when the page is shared on social media. -->
+	<meta property="og:title" content="Page Title">
+	<meta property="og:description" content="A brief summary of the content on the page.">
+	<meta property="og:image" content="https://www.example.com/image.jpg">
+	<meta name="description" content="A brief summary of the content on the page.">
+	<meta name="keywords" content="keyword1, keyword2, keyword3">
+	<!-- This tag controls how search engine bots should crawl and index the webpage. For example, you can use this tag to prevent search engines from indexing certain pages or to specify which pages should be crawled and indexed. -->
+	<meta name="robots" content="index,follow">
+
+	<!-- CSS files -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
+	<!-- JS files -->
+	<script src="assets/js/jquery-3.6.4.js"></script> <!-- jquery-3.6.4 -->
+	<script src="assets/js/bootstrap.js"></script> <!-- bootstrap 5.0.2 -->
+	<script src="assets/js/fontawesome-all.min.js"></script> <!-- font awesome Free 6.4.0 -->
+	<script src="assets/js/script.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<h1 class="text-center text-uppercase">Web Starter</h1>
+
+		<div class="card">
+			<div class="card-body">
+				<strong class="pb-3">This Pack Contains:</strong>
+				<ul>
+					<li>jquery-3.6.4</li>
+					<li>bootstrap 5.0.2</li>
+					<li>Font Awesome Free 6.4.0</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
